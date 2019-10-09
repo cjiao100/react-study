a44ea679a091f74f44a1cba227ffcfa47aeac1a0
cjiao100 <1971416012@qq.com>
2019-10-09 21:36:43 +0800

    增加校验

--- demo01/src/XiaojiejieItem.js
+++ demo01/src/XiaojiejieItem.js
@@ -2,10 +2,11 @@
  * @Author: cjiao100
  * @Date: 2019-09-30 20:32:57
  * @LastEditors: cjiao100
- * @LastEditTime: 2019-10-09 19:03:55
+ * @LastEditTime: 2019-10-09 21:15:29
  * @Description: Do not edit
  */
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class XiaojiejieItem extends Component {
   constructor(props) {
@@ -14,12 +15,22 @@ class XiaojiejieItem extends Component {
   }
 
   render() {
-    return <li onClick={this.handleClick}>{this.props.content}</li>
+    return <li onClick={this.handleClick}>
+      {this.props.name}为你服务： {this.props.content}
+    </li>
   }
 
   handleClick() {
     this.props.deleteItem(this.props.index)
   }
+}
+
+// 校验props的内容
+XiaojiejieItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func
 }
 
 export default XiaojiejieItem
