fa68faaa000befecae33a2e2e7c2b4190bfd0525
cjiao100 <1971416012@qq.com>
2019-12-22 21:58:07 +0800

    使用redux完成删除功能

--- redux/src/store/reducer.js
+++ redux/src/store/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './actionTypes'
+
 const defaultState = {
     inputValue: 'Writee Something',
     list: [
@@ -10,7 +12,7 @@ const defaultState = {
 export default (state = defaultState, action) => {
     // console.log(state,action)
     // Reducer 里只能接受state，不能改变state
-    if (action.type === 'changeInput') {
+    if (action.type === CHANGE_INPUT) {
         // 深度拷贝
         let newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.value
@@ -18,10 +20,17 @@ export default (state = defaultState, action) => {
         return newState
     }
 
-    if(action.type === 'addItem') {
+    if(action.type === ADD_ITEM) {
         let newState = JSON.parse(JSON.stringify(state))
         newState.list.push(newState.inputValue)
         newState.inputValue = ''
+
+        return newState
+    }
+
+    if(action.type === DELETE_ITEM) {
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.item, 1)
 
         return newState
     }
