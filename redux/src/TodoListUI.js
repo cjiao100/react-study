9c2905d20a11f73bee7aa50f4648a11447334043
cjiao100 <1971416012@qq.com>
2020-01-23 21:41:17 +0800

    分离无状态组件

--- redux/src/TodoListUI.js
+++ redux/src/TodoListUI.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // import store from './store'
 // import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './store/actionTypes'
@@ -7,41 +7,39 @@ import React, { Component } from 'react';
 import { Input, Button, List } from 'antd'
 import 'antd/dist/antd.css'
 
-class TodoListUI extends Component {
-
-    render() {
-        return (
-            <div style={{
-                margin: '10px'
-            }}>
-                <div>
-                    <Input
-                        placeholder={this.props.inputValue}
-                        value={this.props.inputValue}
-                        style={{
-                            width: '250px',
-                            marginRight: '10px'
-                        }}
-                        onChange={this.props.changeInputValue}
-                    />
-                    <Button type="primary" onClick={this.props.clickBtn}>增加</Button>
-                </div>
-                <p>{this.props.inputValue}</p>
-                <div style={{
-                    margin: '10px',
-                    width: '300px'
-                }}>
-                    <List
-                        bordered
-                        dataSource={this.props.data}
-                        renderItem={(item, index) => <List.Item onClick={this.props.deleteTodo.bind(this, index)}>
-                            {item}
-                        </List.Item>}
-                    ></List>
-                </div>
+// 使用无状态组件
+const TodoListUI = props => {
+    return (
+        <div style={{
+            margin: '10px'
+        }}>
+            <div>
+                <Input
+                    placeholder={props.inputValue}
+                    value={props.inputValue}
+                    style={{
+                        width: '250px',
+                        marginRight: '10px'
+                    }}
+                    onChange={props.changeInputValue}
+                />
+                <Button type="primary" onClick={props.clickBtn}>增加</Button>
             </div>
-        );
-    }
+            <p>{props.inputValue}</p>
+            <div style={{
+                margin: '10px',
+                width: '300px'
+            }}>
+                <List
+                    bordered
+                    dataSource={props.data}
+                    renderItem={(item, index) => <List.Item onClick={props.deleteTodo.bind(this, index)}>
+                        {item}
+                    </List.Item>}
+                ></List>
+            </div>
+        </div>
+    );
 }
 
 export default TodoListUI
