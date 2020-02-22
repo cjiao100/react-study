641be4bddb88e2ce6e3dd4491f42d2842edea8d4
cjiao100 <1971416012@qq.com>
2020-02-22 21:11:31 +0800

    了解useState

---
+++ react-hooks/src/Example2.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+function Example2 () {
+  const [age, setAge] = useState(20)
+  const [sex, setSex] = useState('男')
+  const [work, setWork] = useState('程序员')
+
+  return (
+    <div>
+      <p>今年{age}岁</p>
+      <p>性别{sex}</p>
+      <p>工作是{work}</p>
+      {/* <button onClick={() => setcount(count + 1)}>Click</button> */}
+    </div>
+  )
+}
+
+export default Example2;
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Example from './Example'
+import Example from './Example2'
 
 ReactDOM.render(<Example />, document.getElementById('root'));
